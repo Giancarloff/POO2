2ff6bcba04de937d5ea1cd220639f8c97711f6b2
Giancarloff <Giancarloff@users.noreply.github.com>
2022-09-20 14:48:40 -0300

    exercicio do elevador

--- elevador/comandoInvalidoException.py
+++ elevador/comandoInvalidoException.py
@@ -1,1 +1,5 @@
-class ComandoInvalidoException( ...
+class ComandoInvalidoException(Exception):
+    
+    def __init__(self, *args: object) -> None:
+        super().__init__("Comando inválido!")
+        
